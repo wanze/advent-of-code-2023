abeb5044e25f6ba0392f0cc687b5a124913ddf9f
Stefan Wanzenried <stefan.wanzenried@devedis.ch>
2023-12-05 00:22:16 +0100

    Add day 3 part 1 and day 4 part 1 + 2

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import {day1Part1, day1Part2} from "./day-01";
 import {day2Part1, day2Part2} from "./day-02";
+import {day3Part1} from "./day-03";
+import {day4Part1, day4Part2} from "./day-04";
 
 console.log(`day01 - part1 : ${day1Part1()}`);
 console.log(`day01 - part2 : ${day1Part2()}`);
 
 console.log(`day02 - part1 : ${day2Part1()}`);
 console.log(`day02 - part2 : ${day2Part2()}`);
+
+console.log(`day03 - part1 : ${day3Part1()}`);
+// console.log(`day03 - part2 : ${day2Part2()}`);
+
+console.log(`day04 - part1 : ${day4Part1()}`);
+console.log(`day04 - part2 : ${day4Part2()}`);
