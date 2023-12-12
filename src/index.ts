2130ec71d8e074171f2e754fe03b69b1e77246be
Stefan Wanzenried <stefan.wanzenried@devedis.ch>
2023-12-12 22:06:59 +0100

    Try day 8 part 1

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import {day1Part1, day1Part2} from "./day-01";
 import {day2Part1, day2Part2} from "./day-02";
 import {day3Part1} from "./day-03";
 import {day4Part1, day4Part2} from "./day-04";
+import {day6Par1, day6Par2} from "./day-06";
+import {day8Part1} from "./day-08";
 
 console.log(`day01 - part1 : ${day1Part1()}`);
 console.log(`day01 - part2 : ${day1Part2()}`);
@@ -14,3 +16,8 @@ console.log(`day03 - part1 : ${day3Part1()}`);
 
 console.log(`day04 - part1 : ${day4Part1()}`);
 console.log(`day04 - part2 : ${day4Part2()}`);
+
+console.log(`day06 - part1 : ${day6Par1()}`);
+console.log(`day06 - part2 : ${day6Par2()}`);
+
+console.log(`day08 - part1 : ${day8Part1()}`);
